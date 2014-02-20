cfd6f13f2a58d00449e740e88d63106790089718
laura-kalondu <udnolak@gmail.com>
2014-02-20 14:06:46 +0300

    add navbar

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/*
+alert ("i am an alert")
+  var message=false
+  if (message==="undefined")
+{
+    alert("true"+message);
+}else
+{
+    alert("False"+message);
+} 
+var button = document.getElementById("my button")
+  console.log("button is" + button)
+button.addEventListener('click',function(){
+alert("button was clicked!");
+});
+*/
+$('#my-button').on("click", function(){
+alert("you clicked")
+})
